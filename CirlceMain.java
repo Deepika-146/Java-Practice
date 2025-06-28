e123495c2307a1e13620ddc5eca864df1bd79ffa
Deepika Palanisamy <deepikapalanisamy146@gmail.com>
2025-06-29 02:12:44 +0530

    Add files via upload

---
+++ CirlceMain.java
@@ -0,0 +1,14 @@
+package day11;
+
+public class CirlceMain {
+	 public static void main(String[] args) {
+	        Circle circle1 = new Circle(7.0);
+	        System.out.println("Circle 1:");
+	        circle1.display();
+	       System.out.println();
+	        Circle circle2 = new Circle();
+	        System.out.println("Circle 2:");
+	        circle2.display();
+	    
+}
+}
