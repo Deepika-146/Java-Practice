e92e9eaacba0e63bb85ed268f9e218fcf5983a11
Deepika Palanisamy <deepikapalanisamy146@gmail.com>
2025-06-29 02:08:50 +0530

    Add files via upload

---
+++ CheckIfACharacterIsVowelOrConsonant.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class CheckIfACharacterIsVowelOrConsonant {
+public static void main(String[] args)
+{
+	char ch='Y';
+	if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch==+'u')
+	{
+		System.out.println("Vowel");
+	}
+	else
+	{
+		System.out.println("consonant");	}
+}
+}
