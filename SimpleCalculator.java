037aaa4f42ea28a694f62ea2a5b8fea6784e248d
Deepika Palanisamy <deepikapalanisamy146@gmail.com>
2025-06-29 15:34:13 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class SimpleCalculator {
+
+	public static void main(String[] args) {
+		int a=9, b=5;
+		char operator='-';
+switch (operator)
+{
+case '+': System.out.println(a+b); break;
+case '-': System.out.println(a-b); break;
+case '*': System.out.println(a*b); break;
+case '/': System.out.println(a/b); break;
+}
+	}
+}
