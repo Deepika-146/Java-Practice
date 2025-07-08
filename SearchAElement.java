2c1118cfa94a11b85e5c21ac29063bfab604362e
Deepika Palanisamy <deepikapalanisamy146@gmail.com>
2025-07-08 16:09:08 +0530

    Add files via upload

---
+++ SearchAElement.java
@@ -0,0 +1,23 @@
+package Day7Arrays;
+
+public class SearchAElement {
+public static void main(String[] args) {
+	int a[]= {10,20,30,40,50,70};
+	int search_element=30;
+	boolean status=true;
+	for (int i=0; i<a.length; i++) {
+		if(a[i]==search_element) {
+			status =true;
+			break;
+		}}
+		if(true)
+		{
+			System.out.println(search_element+"element found");
+		}
+		else if(false)
+		{
+			System.out.println("Not found");
+		}
+	
+}
+}
