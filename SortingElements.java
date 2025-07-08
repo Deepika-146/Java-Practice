bf6eff308def211ec9dd4a412e45e4190d9cce8c
Deepika Palanisamy <deepikapalanisamy146@gmail.com>
2025-07-08 16:10:04 +0530

    Add files via upload

---
+++ SortingElements.java
@@ -0,0 +1,18 @@
+package Day7Arrays;
+
+import java.util.Arrays;
+
+public class SortingElements {
+public static void main(String[] args) {
+	int a[]= {10,20,30,60,80 ,40,50};
+for(int i=0;i<a.length-1;i++){
+	for (int j=0;j<a.length-1;j++){
+	if(a[j]>a[j+1]){
+		int temp=a[j];
+		a[j]=a[j+1];
+		a[j+1]=temp;}
+}}
+System.out.println(Arrays.toString(a));
+
+}
+}
