43385d9de3b6fd6b6a7048ec425bc92ad1778078
Deepika Palanisamy <deepikapalanisamy146@gmail.com>
2025-07-08 16:03:12 +0530

    Add files via upload

---
+++ LargestOfTwoNumbers.java
@@ -0,0 +1,25 @@
+package day4;
+
+public class LargestOfTwoNumbers {
+public static void main(String[] args)
+{
+	int a=25, b=87, x;
+	/*if (a>b) //cannot use ternary operator in if statement
+	{
+		System.out.println(a);
+	}
+	else
+	{
+	x=(a>b)? a:b;
+	System.out.println(x);
+	}*/
+	if (a>b)
+	{
+		System.out.println(a);
+	}
+	else
+	{
+		System.out.println(b);
+	}
+	}
+}
