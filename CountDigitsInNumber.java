2c26de7f42a70cf3d1ee901539a400b07e0f55b0
Deepika Palanisamy <deepikapalanisamy146@gmail.com>
2025-07-08 16:04:17 +0530

    Add files via upload

---
+++ CountDigitsInNumber.java
@@ -0,0 +1,16 @@
+package Day5;
+
+public class CountDigitsInNumber {
+
+	public static void main(String[] args) {
+		int num=12345;
+		int count=0;
+		while (num !=0)
+		{
+			num=num/10;
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
